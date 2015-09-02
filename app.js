4ac005e8e53ea5fa6adc2b9d8aca309ee2fa6a97
niconan <nick.hax@gmail.com>
2015-09-02 15:17:48 -0700

    collection iteration

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ firstApp.controller('FirstCtrl',
     ['$scope',
     function($scope) {
       $scope.title = "Hello from an Angular controller!";
+
+      $scope.cats = ['Kitty',
+                     'Ginger',
+                     'Gizmo',
+                     'Tom',
+                     'Shadow',
+                     'Chloe',
+                     'Smokey'];
     }]);
